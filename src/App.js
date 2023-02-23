222ba0ca6646dae7a35259193154b85975eb031a
Bogdan <bogdan.gugu93@gmail.com>
2023-02-23 08:27:28 +0000

    add event bindings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
-import EventsClass from "./components/EventsClass";
-import EventsFunctional from "./components/EventsFunctional";
+import EventBinding from "./components/EventBinding";
+
 
 function App() {
   return <div className="App">
-    <EventsFunctional />
-    <EventsClass />
+    <EventBinding />
   </div>;
 }
 
