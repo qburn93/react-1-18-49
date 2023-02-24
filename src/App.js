f1cd905cbdc29e172e0cac1bbe6476771f7ec2cc
Bogdan <bogdan.gugu93@gmail.com>
2023-02-24 05:21:00 +0000

    Nesting components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
-import ConditionalRenderingClass from "./components/ConditionalRenderingClass";
-import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional";
+import NestingComponents from "./components/NestingComponents";
+
 
 
 
 function App() {
   return <div className="App">
-    <ConditionalRenderingClass/>
-    <ConditionalRenderingFunctional connected={true} />
+    <NestingComponents />
+    
   </div>;
 }
 
---
+++ src/components/UserData.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function UserData(props) {
+    return (
+        
+        <h1>{props.isLoaded ? 'Data loaded!' : 'Loading ...'}</h1>
+    )
+}
+
+export default UserData
